ce9edb7c421c64b3f6e669cea64f6bea6bcb8491
easing <easing@onyx-international.com>
2024-09-15 18:49:13 +0800

    view locator

--- app/src/main/kotlin/io/github/hellogoogle2000/android/samples/viewlocator/DialogActivity.kt
+++ app/src/main/kotlin/io/github/hellogoogle2000/android/samples/viewlocator/DialogActivity.kt
@@ -4,10 +4,7 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Gravity
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import io.github.hellogoogle2000.android.commons.context.ActivityX.fullscreen
 import io.github.hellogoogle2000.android.samples.databinding.ActivityHomeBinding
 
 class DialogActivity : AppCompatActivity() {
@@ -15,7 +12,6 @@ class DialogActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        nonFullscreen()
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         binding.root.setBackgroundColor(Color.parseColor("#110000FF"))
@@ -31,15 +27,8 @@ class DialogActivity : AppCompatActivity() {
             lp.height = 1000
             lp.gravity = Gravity.CENTER
             window.attributes = lp
+            window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         }
-    }
-
-    private fun nonFullscreen() {
-        fullscreen()
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        val flag = WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
-        window.setFlags(flag, flag)
-        window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 
     private fun initView() {
