e0962c9f23d035f21976a53134feebf831ef6710
easing <easing@onyx-international.com>
2024-09-25 18:18:09 +0800

    popup locator

--- app/src/main/kotlin/x/android/samples/popuplocator/HomeActivity.kt
+++ app/src/main/kotlin/x/android/samples/popuplocator/HomeActivity.kt
@@ -3,16 +3,11 @@ package x.android.samples.popuplocator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import x.android.commons.context.ActivityX.immersive
-import x.android.commons.ui.DockPoint
 import x.android.samples.databinding.ActivityHomeBinding
-import x.android.samples.popuplocator.PopupLocator.getWindowLocation
-import x.android.samples.popuplocator.PopupLocator.show
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
-
-    private lateinit var popup: HomePopup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         immersive()
@@ -23,10 +18,6 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        binding.button.setOnClickListener {
-            popup = HomePopup(this)
-            val windowLocation = popup.getWindowLocation(binding.root, DockPoint.CENTER, DockPoint.CENTER)
-            popup.show(windowLocation, this)
-        }
+
     }
 }
