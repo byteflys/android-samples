fc99b4f4ac9e4194cafd5e3bce950789d32f9598
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-11-05 21:36:28 +0800

    compose

--- app/src/main/kotlin/x/android/samples/compose/HomeActivity.kt
+++ app/src/main/kotlin/x/android/samples/compose/HomeActivity.kt
@@ -3,6 +3,9 @@ package x.android.samples.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import x.android.commons.context.ActivityX.immersive
 
 class HomeActivity : AppCompatActivity() {
@@ -11,7 +14,13 @@ class HomeActivity : AppCompatActivity() {
         immersive()
         super.onCreate(savedInstanceState)
         setContent {
-
+            RootView()
         }
     }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFCDDC39)
+@Composable
+fun RootView() {
+    Text("hello")
 }
