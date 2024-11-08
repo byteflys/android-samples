01cde43076b9d37cbc0a8ee7b2659d84dc3bbba3
easing <easing@onyx-international.com>
2024-11-08 09:45:11 +0800

    compose CompositionLocal

--- app/src/main/kotlin/x/android/samples/compose/HomeActivity.kt
+++ app/src/main/kotlin/x/android/samples/compose/HomeActivity.kt
@@ -3,9 +3,6 @@ package x.android.samples.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import x.android.commons.context.ActivityX.immersive
 
 class HomeActivity : AppCompatActivity() {
@@ -13,14 +10,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         immersive()
         super.onCreate(savedInstanceState)
-        setContent {
-            RootView()
-        }
+        setContent { CompositionLocalView(true) }
     }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFCDDC39, showSystemUi = false)
-@Composable
-fun RootView() {
-    Text("hello")
 }
