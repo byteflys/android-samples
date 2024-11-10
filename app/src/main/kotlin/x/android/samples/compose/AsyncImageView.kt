179f99cf047030aeab7249672e744915313f1f0e
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-11-10 12:06:12 +0800

    compose async image

--- app/src/main/kotlin/x/android/samples/compose/AsyncImageView.kt
+++ app/src/main/kotlin/x/android/samples/compose/AsyncImageView.kt
@@ -1,20 +1,11 @@
 package x.android.samples.compose
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.request.crossfade
 
 @Preview
 @Composable
 fun AsyncImageView() {
-    AsyncImage(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data("https://www.sanguosha.cn/new_index_pc/img/sgs_code.jpg")
-            .crossfade(true)
-            .build(),
-        contentDescription = null
-    )
+    AsyncImage(model = "https://www.sanguosha.cn/new_index_pc/img/sgs_code.jpg", contentDescription = null)
 }
