f25d8a23e221d0e0b359d835a3f05601fd39b687
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-11-25 21:52:15 +0800

    compose VerticalScrollBarImpl

--- app/src/main/kotlin/x/android/samples/compose/VerticalScrollBarImpl.kt
+++ app/src/main/kotlin/x/android/samples/compose/VerticalScrollBarImpl.kt
@@ -2,14 +2,18 @@ package x.android.samples.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,9 +31,9 @@ import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 
-private val parentWidth = 500.dp
+private val parentWidth = 300.dp
 private val parentHeight = 200.dp
-private val contentWidth = 400.dp
+private val contentWidth = 100.dp
 private val contentHeight = 400.dp
 
 class RectOutlineShape(val left: Float, val top: Float, val width: Float, val height: Float) : Shape {
@@ -39,25 +43,29 @@ class RectOutlineShape(val left: Float, val top: Float, val width: Float, val he
 }
 
 @Composable
-private fun ResizableBox() {
-    val contentHeightPx = with(LocalDensity.current) { contentHeight.toPx() }
-    val gradient = Brush.linearGradient(0f to Color.Black, 0.5f to Color.Red, 1f to Color.Black, start = Offset(0f, 0f), end = Offset(0f, contentHeightPx))
-    val scrollState = rememberScrollState()
-    val clipOutline = with(LocalDensity.current) {
-        RectOutlineShape(0f, contentHeight.minus(parentHeight).toPx() / 2, contentWidth.toPx(), parentHeight.toPx())
-    }
-    Box(Modifier.background(Color.Yellow).width(parentWidth).height(parentHeight).scrollable(scrollState, Orientation.Vertical)) {
-        Box(
-            Modifier.align(Alignment.Center).requiredSize(contentWidth, contentHeight).clip(clipOutline)
-                .offset { IntOffset(0, clipOutline.top.toInt()) }
-                .offset { IntOffset(0, -scrollState.value) }
-                .background(gradient)
-        )
+private fun VerticalScrollBarImpl() {
+    with(LocalDensity.current) {
+        val gradient = Brush.linearGradient(0f to Color.Black, 0.5f to Color.Red, 1f to Color.Black, start = Offset(0f, 0f), end = Offset(0f, contentHeight.toPx()))
+        var offset by remember { mutableFloatStateOf(0f) }
+        val scrollState = rememberScrollableState { delta ->
+            offset = (offset + delta).coerceIn(parentHeight.minus(contentHeight).toPx(), 0f)
+            println("$offset $delta")
+            return@rememberScrollableState delta
+        }
+        val clipOutline = RectOutlineShape(0f, contentHeight.minus(parentHeight).toPx() / 2, contentWidth.toPx(), parentHeight.toPx())
+        Box(Modifier.background(Color.Yellow).width(parentWidth).height(parentHeight).scrollable(scrollState, Orientation.Vertical)) {
+            Box(
+                Modifier.align(Alignment.Center).requiredSize(contentWidth, contentHeight).clip(clipOutline)
+                    .offset { IntOffset(0, clipOutline.top.toInt()) }
+                    .offset { IntOffset(0, offset.toInt()) }
+                    .background(gradient)
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun Compose20() {
-    ResizableBox()
+    VerticalScrollBarImpl()
 }
