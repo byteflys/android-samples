db710970397e5b66121a854b6cd956b461e1cf20
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-12-08 22:46:53 +0800

    compose state SideEffect LaunchedEffect

--- app/src/main/kotlin/x/android/samples/compose/Temp.kt
+++ app/src/main/kotlin/x/android/samples/compose/Temp.kt
@@ -1,36 +1,43 @@
 package x.android.samples.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.mapSaver
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
-data class User(val id: String, val name: String)
-
-private val UserMapSaver = mapSaver(
-    save = { mapOf("id" to it.id, "name" to it.name) },
-    restore = {
-        val id = it["id"] as String
-        val name = it["name"] as String
-        User(id, name)
-    }
-)
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 @Composable
 private fun Temp() {
-    var user by rememberSaveable(stateSaver = UserMapSaver) { mutableStateOf(User("1", "A")) }
-    Text("${user.name}")
+    var key by remember { mutableIntStateOf(1) }
+    var width by remember { mutableStateOf(100.dp) }
+    Box(Modifier.width(width).height(50.dp).background(Color.Yellow)) {
+        println("composable recomposed")
+        LaunchedEffect(key) {
+            println("coroutine launched")
+            suspendCancellableCoroutine<Unit> {
+                it.invokeOnCancellation { println("coroutine cancelled") }
+            }
+            while (true) delay(100L)
+        }
+    }
     Button(onClick = {
-        user = User("2", "B")
+        width += 1.dp
+    }, Modifier.width(100.dp).height(50.dp)) { }
+    Button(onClick = {
+        key++
     }, Modifier.width(100.dp).height(50.dp)) { }
 }
 
