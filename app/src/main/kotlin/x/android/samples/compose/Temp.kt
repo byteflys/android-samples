4ed8f86457c618d41119e74b545b7bb55d1fc65c
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-12-08 20:48:09 +0800

    compose state remember with keys

--- app/src/main/kotlin/x/android/samples/compose/Temp.kt
+++ app/src/main/kotlin/x/android/samples/compose/Temp.kt
@@ -1,28 +1,36 @@
 package x.android.samples.compose
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import x.kotlin.commons.string.UUID
+
+data class User(val id: String, val name: String)
 
 @Composable
 private fun Temp() {
-    var count1 by remember { mutableIntStateOf(1) }
-    Button(onClick = { count1++ }, Modifier.width(200.dp).height(50.dp)) {
-        if (count1 % 3 == 1) {
-            var count2 by remember { mutableIntStateOf(1) }
-            Text("$count1 $count2", Modifier.clickable { count2++ })
-        }
+    var id by remember { mutableStateOf("1") }
+    var user by remember(id) {
+        println("execute computation block")
+        mutableStateOf(User("1", "A"))
     }
+    Text("${user.name}", Modifier.graphicsLayer { println("recompose") })
+    Button(onClick = {
+        id = UUID.short()
+    }, Modifier.width(100.dp).height(50.dp)) { }
+    Button(onClick = {
+        user = User("2", "B")
+    }, Modifier.width(100.dp).height(50.dp)) { }
 }
 
 @Preview
