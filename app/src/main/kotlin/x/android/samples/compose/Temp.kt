666a504e07bccb877d67fa4a493300283a93c9ea
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-12-08 21:32:27 +0800

    compose state rememberSaveable Parcelable

--- app/src/main/kotlin/x/android/samples/compose/Temp.kt
+++ app/src/main/kotlin/x/android/samples/compose/Temp.kt
@@ -1,5 +1,6 @@
 package x.android.samples.compose
 
+import android.os.Parcelable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -7,27 +8,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import x.kotlin.commons.string.UUID
+import kotlinx.parcelize.Parcelize
 
-data class User(val id: String, val name: String)
+@Parcelize
+data class User(val id: String, val name: String) : Parcelable
 
 @Composable
 private fun Temp() {
-    var id by remember { mutableStateOf("1") }
-    var user by remember(id) {
-        println("execute computation block")
-        mutableStateOf(User("1", "A"))
-    }
-    Text("${user.name}", Modifier.graphicsLayer { println("recompose") })
-    Button(onClick = {
-        id = UUID.short()
-    }, Modifier.width(100.dp).height(50.dp)) { }
+    var user by rememberSaveable { mutableStateOf(User("1", "A")) }
+    Text("${user.name}")
     Button(onClick = {
         user = User("2", "B")
     }, Modifier.width(100.dp).height(50.dp)) { }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,6 +35,7 @@ buildscript {
 plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.0.21" apply false
     id("com.android.application") version "8.7.0" apply false
     id("com.google.devtools.ksp") version "2.0.10-1.0.24" apply false
     id("com.vanniktech.maven.publish") version "0.29.0" apply false
