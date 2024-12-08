8d96589b84dc75d97a71380c5c69227ceaf69fe6
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-12-08 21:41:04 +0800

    compose state rememberSaveable listSaver

--- app/src/main/kotlin/x/android/samples/compose/Temp.kt
+++ app/src/main/kotlin/x/android/samples/compose/Temp.kt
@@ -1,6 +1,5 @@
 package x.android.samples.compose
 
-import android.os.Parcelable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -8,19 +7,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
-data class User(val id: String, val name: String) : Parcelable
+data class User(val id: String, val name: String)
+
+private val UserListSaver = listSaver<User, Any>(
+    save = { listOf(it.id, it.name) },
+    restore = {
+        val id = it[0] as String
+        val name = it[1] as String
+        User(id, name)
+    }
+)
 
 @Composable
 private fun Temp() {
-    var user by rememberSaveable { mutableStateOf(User("1", "A")) }
+    var user by rememberSaveable(stateSaver = UserListSaver) { mutableStateOf(User("1", "A")) }
     Text("${user.name}")
     Button(onClick = {
         user = User("2", "B")
