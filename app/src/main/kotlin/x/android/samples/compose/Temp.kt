f5bf20f0242a8a61355c01aec0a2f13cfa432da8
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-12-08 19:54:56 +0800

    compose state remember scope

--- app/src/main/kotlin/x/android/samples/compose/Temp.kt
+++ app/src/main/kotlin/x/android/samples/compose/Temp.kt
@@ -1,5 +1,8 @@
 package x.android.samples.compose
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -7,16 +10,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kotlin.random.Random
+import androidx.compose.ui.unit.dp
 
 @Composable
 private fun Temp() {
-    var count by remember { mutableIntStateOf(1) }
-    val onClick = {
-        count = Random.nextInt(1, 3)
+    var count1 by remember { mutableIntStateOf(1) }
+    Button(onClick = { count1++ }, Modifier.width(200.dp).height(50.dp)) {
+        if (count1 % 3 == 1) {
+            var count2 by remember { mutableIntStateOf(1) }
+            Text("$count1 $count2", Modifier.clickable { count2++ })
+        }
     }
-    Button(onClick = onClick) { Text(count.toString()) }
 }
 
 @Preview
