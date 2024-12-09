677ae197ed7ece0b0eb04fb04c3ed6dab2f78fd7
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-12-09 23:48:32 +0800

    compose state SideEffect DisposableEffect

--- app/src/main/kotlin/x/android/samples/compose/Counter.kt
+++ app/src/main/kotlin/x/android/samples/compose/Counter.kt
@@ -3,22 +3,34 @@ package x.android.samples.compose
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 private fun Counter() {
     var count by remember { mutableIntStateOf(0) }
-    SideEffect {
-        println("recomposed $count")
+    DisposableEffect(count) {
+        println("scope created")
+        val scope = CoroutineScope(Dispatchers.Default)
+        val job = scope.launch {
+            while (true) delay(1000)
+        }
+        onDispose {
+            println("scope disposed")
+            job.cancel()
+        }
     }
-    Button(onClick = { count++ }) {
-        Text("$count")
-    }
+    Button(onClick = { }) { Text("1") }
+    Button(onClick = { count++ }) { Text("2") }
 }
 
 @Preview
