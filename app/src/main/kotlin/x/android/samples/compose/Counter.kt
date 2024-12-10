7db72a4840be8caec0f6af92edbf2d1ab5a2b1d8
easing <easing@onyx-international.com>
2024-12-10 10:08:37 +0800

    compose state SideEffect produceState

--- app/src/main/kotlin/x/android/samples/compose/Counter.kt
+++ app/src/main/kotlin/x/android/samples/compose/Counter.kt
@@ -1,18 +1,24 @@
 package x.android.samples.compose
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
 
 @Composable
 private fun Counter() {
-    var count1 by remember { mutableIntStateOf(0) }
-    var count2 by remember { mutableIntStateOf(0) }
-    val count by remember { derivedStateOf { count1 + count2 } }
+    var foregroundColor by remember { mutableStateOf(Color.Red) }
+    val backgroundColor by produceState(Color.White) {
+        delay(1000L)
+        foregroundColor.let {
+            value = Color(0.1f, it.red, it.green, it.blue)
+        }
+    }
 }
 
 @Preview
