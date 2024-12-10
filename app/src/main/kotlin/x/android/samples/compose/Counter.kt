20c9812734c8fa37e48a8207dbd416781c499671
easing <easing@onyx-international.com>
2024-12-10 12:03:04 +0800

    compose state SideEffect rememberUpdatedState

--- app/src/main/kotlin/x/android/samples/compose/Counter.kt
+++ app/src/main/kotlin/x/android/samples/compose/Counter.kt
@@ -1,34 +1,36 @@
 package x.android.samples.compose
 
-import androidx.compose.material3.Button
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
 
 @Composable
-private fun Counter() {
-    val coroutineScope = rememberCoroutineScope()
-    var count by remember { mutableIntStateOf(0) }
-    val onClick: () -> Unit = {
-        coroutineScope.launch {
-            delay(3000)
-            count++
-        }
+private fun Counter(foregroundColor: Color, modifier: Modifier) {
+    val foregroundColor by rememberUpdatedState(foregroundColor)
+    val backgroundColor = remember(foregroundColor) {
+        val origin = foregroundColor
+        Color(origin.red, origin.green, origin.blue, 0.2f)
     }
-    Button(onClick = onClick) {
-        Text("$count")
+    Box(modifier = modifier.background(backgroundColor)) {
+        Text("1", color = foregroundColor)
     }
 }
 
 @Preview
 @Composable
 fun Compose42() {
-    Counter()
+    var foregroundColor by remember { mutableStateOf(Color.Red) }
+    Counter(foregroundColor, Modifier.size(100.dp).clickable { foregroundColor = Color.Blue })
 }
