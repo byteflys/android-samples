e6680283b7f9f64235e3bcfcd986d7f093539f21
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-12-11 23:06:02 +0800

    compose ComposeView

--- app/src/main/kotlin/x/android/samples/compose/HomeActivity.kt
+++ app/src/main/kotlin/x/android/samples/compose/HomeActivity.kt
@@ -1,23 +1,24 @@
 package x.android.samples.compose
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import x.android.samples.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Column(Modifier.fillMaxSize().background(Color.LightGray), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-                Compose42()
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.composeView.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnDetachedFromWindow)
+        binding.composeView.setContent {
+            Canvas(Modifier.fillMaxSize()) {
+                drawRect(Color.Red, size = size)
             }
         }
     }
